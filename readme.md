31a5310f494d3f706d5bcd58eac64ee345658a05
OnurSefa <onursefa_ozcibik@hotmail.com>
2024-08-02 14:32:56 +0300

    U-Net implementation

---
+++ readme.md
@@ -0,0 +1,8 @@
+# U-Net Depth Estimation
+
+
+I used [this](https://github.com/milesial/Pytorch-UNet/blob/master/unet/unet_model.py)
+resource as a reference to implement the U-net model structure.
+
+I used [NYU Depth V2](https://www.kaggle.com/datasets/soumikrakshit/nyu-depth-v2)
+dataset that is stored in kaggle.
--- unet_model.py
+++ unet_model.py
@@ -1,11 +1,91 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None, kernel_size=3, padding=1, bias=False):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=kernel_size, padding=padding, bias=bias),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=bias),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=2, stride=2, padding=0):
+        super().__init__()
+
+        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=kernel_size, stride=stride, padding=padding)
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is chw
+        diff_y = x2.size()[2] - x1.size()[2]
+        diff_x = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diff_x // 2, diff_x - diff_x // 2, diff_y // 2, diff_y - diff_y // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=1):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size)
+
+    def forward(self, x):
+        return self.conv(x)
 
 
 class UNet(nn.Module):
     def __init__(self):
         super(UNet, self).__init__()
 
+        self.down0 = DoubleConv(3, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 1024)
+        self.up0 = Up(1024, 512)
+        self.up1 = Up(512, 256)
+        self.up2 = Up(256, 128)
+        self.up3 = Up(128, 64)
+        self.out = OutConv(64, 1)
+
     def forward(self, x):
+        x1 = self.down0(x)
+        x2 = self.down1(x)
+        x3 = self.down2(x)
+        x4 = self.down3(x)
+        x5 = self.down4(x)
+        x = self.up0(x5, x4)
+        x = self.up1(x, x3)
+        x = self.up2(x, x2)
+        x = self.up3(x, x1)
+        x = self.out(x)
         return x
 
