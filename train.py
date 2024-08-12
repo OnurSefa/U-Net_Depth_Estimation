0ef187c0809c4cf3c0b7330bf76c4280a7e78592
OnurSefa <onursefa_ozcibik@hotmail.com>
2024-08-13 00:32:48 +0300

    mlflow implementation

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from torch.utils.data import DataLoader
 from loss import depth_loss
 import torch.optim as o
 import torch
+import mlflow
 
 
 def train(model, optimizer, loss_function, epoch_count, model_dir, model_prefix, save_interval, batch_size, image_names_path, normalizer_paths, shuffle=True):
@@ -22,7 +23,10 @@ def train(model, optimizer, loss_function, epoch_count, model_dir, model_prefix,
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
     save_loss = 0
+    epoch_loss = 0
     save_iteration = 0
+    epoch_iteration = 0
+    step = 0
 
     for epoch in range(epoch_count):
         for i, data in enumerate(data_loader):
@@ -34,27 +38,46 @@ def train(model, optimizer, loss_function, epoch_count, model_dir, model_prefix,
             optimizer.step()
 
             print(f'{epoch} - {i} - loss: {loss.item():.4f}')
+
+            mlflow.log_metric('batch loss', loss.item(), step=step)
+
             save_loss += loss.item()
+            epoch_loss += loss.item()
             save_iteration += 1
+            epoch_iteration += 1
             if i % save_interval == 0:
                 torch.save(model, f'{model_dir}/{model_prefix}_{epoch}_{i}.pth')
+                mlflow.log_metric('save loss', save_loss / save_iteration, step=step)
                 print(f"SAVE LOSS {epoch} - {i}: {save_loss/save_iteration}")
                 save_loss = 0
                 save_iteration = 0
                 print()
+            step += 1
+        mlflow.log_metric('epoch loss', epoch_loss/epoch_iteration, step=step)
+        epoch_loss = 0
+        epoch_iteration = 0
 
 
 if __name__ == '__main__':
     m = UNet()
-    learning_rate = 0.1
+    learning_rate = 0.000001
     optim = o.Adam(m.parameters(), lr=learning_rate)
     lf = depth_loss
     ec = 10
     md = 'models'
-    mp = '001'
+    mp = '002'
     si = 30
     bs = 16
     im_names_path = 'data/train_names.json'
     norm_paths = ['data/image_means.pth', 'data/image_stds.pth', 'data/depth_means.pth', 'data/depth_stds.pth']
+
+    mlflow.start_run(run_name=f'{mp}')
+    mlflow.log_param('learning rate', learning_rate)
+    mlflow.log_param('batch size', bs)
+    mlflow.log_param('loss function', 'depth')
+    mlflow.log_param('model name', mp)
+    mlflow.log_param('model initiated', 'scratch')
+
+    mlflow.log_param('notes', 'training with lower learning rate')
     train(m, optim, lf, ec, md, mp, si, bs, im_names_path, norm_paths)
 
