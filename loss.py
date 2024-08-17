ebb3cde4245cd162ae0ee2706166201781cb01a6
OnurSefa <onursefa_ozcibik@hotmail.com>
2024-08-17 20:16:49 +0300

    Experiments

--- loss.py
+++ loss.py
@@ -2,24 +2,20 @@ import torch
 import torch.nn.functional as F
 
 
-def depth_loss(y_true, y_pred, theta=0.1, max_depth_val=1):
+def depth_loss(y_true, y_pred, alpha=0., beta=0., theta=0., max_depth_val=1.):
     # point-wise depth
     l_depth = torch.mean(torch.abs(y_true - y_pred))
 
     # edge loss
-    # dy_true, dx_true = image_gradients(y_true)
-    # dy_pred, dx_pred = image_gradients(y_pred)
-    # l_edges = torch.mean(torch.abs(dy_pred - dy_true) + torch.abs(dx_pred - dx_true))
+    dy_true, dx_true = image_gradients(y_true)
+    dy_pred, dx_pred = image_gradients(y_pred)
+    l_edges = torch.mean(torch.abs(dy_pred - dy_true) + torch.abs(dx_pred - dx_true))
 
     # SSIM
-    # l_ssim = torch.mean(torch.clamp((1 - ssim(y_true, y_pred, max_depth_val)) * 0.5, 0, 1))
+    l_ssim = torch.mean(torch.clamp((1 - ssim(y_true, y_pred, max_depth_val)) * 0.5, 0, 1))
 
-    # w0 = theta
-    # w1 = 1.0
-    # w2 = 1.0
 
-    # return (w0 * l_depth) + (w1 * l_edges) + (w2 * l_ssim)
-    return l_depth
+    return (alpha * l_depth) + (beta * l_edges) + (theta * l_ssim)
 
 
 def image_gradients(x):
--- train.py
+++ train.py
@@ -1,20 +1,24 @@
 from unet_model import UNet
-from loader import DepthDataset, Normalizer
+from loader import DepthDataset
 from torch.utils.data import DataLoader
 from loss import depth_loss
 import torch.optim as o
 import torch
 import mlflow
+import random
 
 
-def train(model, optimizer, loss_function, epoch_count, model_dir, model_prefix, save_interval, batch_size, image_names_path, normalizer_paths, shuffle=True):
+def train(model, optimizer, loss_function, epoch_count, model_dir, model_prefix, save_interval, batch_size, image_names_path, shuffle=True, alpha=0., beta=0., theta=0.):
 
     if torch.backends.mps.is_available():
         device = torch.device("mps")
+        print('device is mps')
     elif torch.cuda.is_available():
         device = torch.device("cuda")
+        print('device is cuda')
     else:
         device = torch.device('cpu')
+        print('device is cpu')
 
     model = model.to(device)
     model.train()
@@ -33,7 +37,7 @@ def train(model, optimizer, loss_function, epoch_count, model_dir, model_prefix,
             optimizer.zero_grad()
             images, depths = data
             depth_predictions = model(images)
-            loss = loss_function(depths, depth_predictions)
+            loss = loss_function(depths, depth_predictions, alpha, beta, theta)
             loss.backward()
             optimizer.step()
 
@@ -45,7 +49,7 @@ def train(model, optimizer, loss_function, epoch_count, model_dir, model_prefix,
             epoch_loss += loss.item()
             save_iteration += 1
             epoch_iteration += 1
-            if i % save_interval == 0:
+            if i % save_interval == 0 and i != 0:
                 torch.save(model, f'{model_dir}/{model_prefix}_{epoch}_{i}.pth')
                 mlflow.log_metric('save loss', save_loss / save_iteration, step=step)
                 print(f"SAVE LOSS {epoch} - {i}: {save_loss/save_iteration}")
@@ -59,25 +63,34 @@ def train(model, optimizer, loss_function, epoch_count, model_dir, model_prefix,
 
 
 if __name__ == '__main__':
-    m = UNet()
-    learning_rate = 0.000001
-    optim = o.Adam(m.parameters(), lr=learning_rate)
     lf = depth_loss
-    ec = 10
+    ec = 1
     md = 'models'
-    mp = '002'
-    si = 30
-    bs = 16
-    im_names_path = 'data/train_names.json'
-    norm_paths = ['data/image_means.pth', 'data/image_stds.pth', 'data/depth_means.pth', 'data/depth_stds.pth']
+    si = 80
+    bs = 8
+    im_names_path = 'train_names.json'
 
-    mlflow.start_run(run_name=f'{mp}')
-    mlflow.log_param('learning rate', learning_rate)
-    mlflow.log_param('batch size', bs)
-    mlflow.log_param('loss function', 'depth')
-    mlflow.log_param('model name', mp)
-    mlflow.log_param('model initiated', 'scratch')
+    for mp_index in range(5, 100):
+        m = UNet()
 
-    mlflow.log_param('notes', 'training with lower learning rate')
-    train(m, optim, lf, ec, md, mp, si, bs, im_names_path, norm_paths)
+        learning_rate = 10 ** random.uniform(-6, 1)
+        optim = o.Adam(m.parameters(), lr=learning_rate)
 
+        a = 10 ** random.uniform(-2, 1)
+        b = 10 ** random.uniform(-2, 1)
+        c = 10 ** random.uniform(-2, 1)
+
+        mp = f'{mp_index:03}'
+        mlflow.start_run(run_name=f'{mp}')
+        mlflow.log_param('learning rate', learning_rate)
+        mlflow.log_param('batch size', bs)
+        mlflow.log_param('loss function', 'depth-gradient-ssim')
+        mlflow.log_param('model name', mp)
+        mlflow.log_param('model initiated', 'scratch')
+        mlflow.log_param('alpha', a)
+        mlflow.log_param('beta', b)
+        mlflow.log_param('theta', c)
+
+        mlflow.log_param('notes', 'training with multiple random hyper parameters')
+        train(m, optim, lf, ec, md, mp, si, bs, im_names_path, True, a, b, c)
+        mlflow.end_run()
