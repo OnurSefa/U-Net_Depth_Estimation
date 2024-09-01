43f1636e6c9fd883520839c6e668732a1a2a4ca8
OnurSefa <onursefa_ozcibik@hotmail.com>
2024-09-01 14:26:53 +0300

    Update #2 data_ops update

--- data_ops.py
+++ data_ops.py
@@ -1,31 +1,6 @@
 import random
-
-import numpy
-
-from fill_depth_colorization import fill_depth_map, advanced_fill_depth_map, fill_depth_colorization
 import json
 import os
-from PIL import Image
-import numpy as np
-import torchvision.transforms as T
-from torchvision.transforms.functional import pil_to_tensor
-import torch
-
-
-def split_data(csv_path, train_path, test_path, train_size=40000, random_seed=42):
-    if random_seed:
-        random.seed(random_seed)
-    csv = pd.read_csv(csv_path)
-    names = []
-    for index, row in csv.iterrows():
-        names.append([row[0], row[1]])
-
-    random.shuffle(names)
-
-    with open(train_path, 'w') as f:
-        json.dump({"names": names[:train_size]}, f, indent=6)
-    with open(test_path, 'w') as f:
-        json.dump({"names": names[train_size:]}, f, indent=6)
 
 
 def find_original_datapoints(in_dir, out_path):
@@ -65,34 +40,6 @@ def select_original_portion(in_path, out_path, ratio, random_seed=42):
         json.dump(data_points[:selected_count], f, indent=6)
 
 
-def fill_depth_images(dp_path, out_dir):
-    with open(dp_path, 'r') as f:
-        data_points = json.load(f)
-
-    # tensor_to_pil = T.ToPILImage(mode="L")
-    for image_path, depth_path in data_points:
-        image = Image.open(image_path)
-        image = np.array(image, dtype=np.float32)
-        image = image - image.min()
-        image = image / image.max()
-        depth = Image.open(depth_path)
-        depth = np.array(depth, dtype=np.float32)
-        depth = depth - depth.min()
-        depth = depth / depth.max()
-        depth_image = Image.fromarray(depth, mode='F')
-        depth_name = depth_path.split('/')[-1][:-4] + "-0.tiff"
-        depth_image.save(os.path.join(out_dir, depth_name))
-        processed = fill_depth_colorization(image, depth)
-        processed = processed - np.min(processed)
-        processed = processed / np.max(processed)
-        processed = Image.fromarray(processed, mode='F')
-        depth_name = depth_path.split('/')[-1][:-4] + "-1.tiff"
-        save_name = os.path.join(out_dir, depth_name)
-        print(save_name)
-        processed.save(save_name)
-
-
 if __name__ == '__main__':
     find_original_datapoints('data/original', 'original_paths.json')
     select_original_portion('original_paths.json', 'original_paths_portion.json', 0.5, random_seed=42)
-    # fill_depth_images('original_paths_portion.json', 'data/original_processed')
