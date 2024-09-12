44cb2e345ab31c08b2d399a7cb533f88c0f299b2
OnurSefa <onursefa_ozcibik@hotmail.com>
2024-09-12 12:24:09 +0300

    Update #2 - fine-tune

--- evaluate.py
+++ evaluate.py
@@ -127,6 +127,66 @@ def create_comparison_table(model_path_original, model_path_fine_tuned, data_ind
 
     plt.savefig(out_path, dpi=300, bbox_inches='tight')
 
+
+def create_comparison_table_multiple_model(names, in_dir, data_indices, image_names_path, out_path):
+    if torch.backends.mps.is_available():
+        device = torch.device("mps")
+    elif torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device('cpu')
+
+    dataset = DepthDataset(image_names_path, device=device, normalize=True, mode='inference')
+    models = []
+    for name in names:
+        model = torch.load(f"{in_dir}/{name}.pth", map_location=device).eval()
+        models.append(model)
+    cpu = torch.device('cpu')
+
+    transform = T.ToPILImage(mode='L')
+    transform_rgb = T.ToPILImage(mode='RGB')
+
+    height_ratios = [0.1]
+    for _ in data_indices:
+        height_ratios.append(4)
+
+    fig, axs = plt.subplots(len(data_indices) + 1, 2+len(models), figsize=(8*(2+len(models)), len(data_indices)*6+0.6), gridspec_kw={'height_ratios': height_ratios})
+    plt.subplots_adjust(hspace=0.05, wspace=0.05)
+    axs[0, 0].text(0.5, 0.5, 'RGB Image', ha='center', va='center', fontweight='bold')
+    axs[0, 1].text(0.5, 0.5, 'Ground Truth', ha='center', va='center', fontweight='bold')
+    for i in range(len(names)):
+        axs[0, 2+i].text(0.5, 0.5, names[i], ha='center', va='center', fontweight='bold')
+    for ax in axs[0]:
+        ax.axis('off')
+
+    for i, data_index in enumerate(data_indices):
+        data = dataset[data_index]
+        image, image_f, image_o, depth, path = data
+        input_images = []
+        for im in [image, image_f]:
+            input_images.append(im.unsqueeze(0))
+        axs[i+1, 0].imshow(transform_rgb(image_o))
+        axs[i+1, 0].axis('off')
+
+        depth = (depth - torch.min(depth)) / (torch.max(depth) - torch.min(depth))
+        axs[i+1, 1].imshow(transform(depth), cmap='viridis')
+        axs[i+1, 1].axis('off')
+
+        for m, model in enumerate(models):
+            depth_predictions = torch.zeros_like(depth).to(cpu).unsqueeze(0)
+            for image_type, input_image in enumerate(input_images):
+                if image_type == 1:
+                    depth_predictions += hflip(model(input_image)).to(cpu)
+                else:
+                    depth_predictions += model(input_image).to(cpu)
+            depth_predictions /= len(input_images)
+            depth_predictions = (depth_predictions - torch.min(depth_predictions)) / (torch.max(depth_predictions) - torch.min(depth_predictions))
+            output_img = transform(depth_predictions[0, :, :, :])
+            axs[i+1, 2+m].imshow(output_img, cmap='viridis')
+            axs[i+1, 2+m].axis('off')
+
+    plt.savefig(out_path, dpi=300, bbox_inches='tight')
+
 if __name__ == '__main__':
     # mps = []
     # names = ["037_19_100"]
@@ -135,7 +195,11 @@ if __name__ == '__main__':
     #
     # evaluate('../data/depth_data/evaluate_finetune', mps, '../data/depth_data/fine_tune_test.json')
 
-    create_comparison_table('../data/depth_models/030_14_4500.pth', None, [124, 132, 206, 357, 358], '../data/depth_data/original_paths_portion_test.json', '../data/depth_data/evaluate.png')
-    create_comparison_table(None, '../data/depth_models/037_19_100.pth', [24, 74, 114, 167, 172], '../data/depth_data/fine_tune_test.json', '../data/depth_data/evaluate_finetune.png')
-    create_comparison_table('../data/depth_models/030_14_4500.pth', '../data/depth_models/037_19_100.pth', [24, 74, 114, 167, 172], '../data/depth_data/fine_tune_test.json', '../data/depth_data/final_comparison_fine_tune_data.png')
-    create_comparison_table('../data/depth_models/030_14_4500.pth', '../data/depth_models/037_19_100.pth', [124, 132, 206, 357, 358], '../data/depth_data/original_paths_portion_test.json', '../data/depth_data/final_comparison_raw_data.png')
+    # create_comparison_table('../data/depth_models/030_14_4500.pth', None, [124, 132, 206, 357, 358], '../data/depth_data/original_paths_portion_test.json', '../data/depth_data/evaluate.png')
+    # create_comparison_table(None, '../data/depth_models/037_19_100.pth', [24, 74, 114, 167, 172], '../data/depth_data/fine_tune_test.json', '../data/depth_data/evaluate_finetune.png')
+    # create_comparison_table('../data/depth_models/030_14_4500.pth', '../data/depth_models/037_19_100.pth', [24, 74, 114, 167, 172], '../data/depth_data/fine_tune_test.json', '../data/depth_data/final_comparison_fine_tune_data.png')
+    # create_comparison_table('../data/depth_models/030_14_4500.pth', '../data/depth_models/037_19_100.pth', [124, 132, 206, 357, 358], '../data/depth_data/original_paths_portion_test.json', '../data/depth_data/final_comparison_raw_data.png')
+
+    model_names = ["037_19_100", "040_99_140", "041_99_140", "042_99_140", "045_99_140", "046_99_140", "047_99_140"]
+    model_dir = "../data/depth_models"
+    create_comparison_table_multiple_model(model_names, model_dir, [24, 52, 74, 114, 167, 172, 189, 199], '../data/depth_data/fine_tune_test.json', '../data/depth_data/final_comparison_fine_tune_data_2.png')
--- train.py
+++ train.py
@@ -64,33 +64,34 @@ def train(model, optimizer, loss_function, epoch_count, model_dir, model_prefix,
 
 if __name__ == '__main__':
     lf = depth_loss
-    ec = 20
+    ec = 100
     md = '../data/depth_models'
-    si = 100
+    si = 140
     bs = 8
     im_names_path = '../data/depth_data/fine_tune_train.json'
 
-    for mp_index in range(31, 40):
+    for mp_index in range(45, 48):
         # m = UNet()
-        m = torch.load('../data/depth_models/030_14_4500.pth')
-        learning_rate = 10 ** random.uniform(-5, -3)
-        # learning_rate = 0.00033
+        m = torch.load('../data/depth_models/037_19_100.pth')
+        if mp_index == 45:
+            learning_rate = 3.8374156626590274e-05
+            a = 4.2523889554143555
+            b = 1.2242428837260741
+            c = 80.30816839128433
+        else:
+            learning_rate = 10 ** random.uniform(-6, -4)
+            a = 10 ** random.uniform(0, 2)
+            b = 10 ** random.uniform(0, 2)
+            c = 10 ** random.uniform(0, 2)
+
         optim = o.Adam(m.parameters(), lr=learning_rate)
-
-        a = 10 ** random.uniform(-3, -1)
-        b = 10 ** random.uniform(-3, -1)
-        c = 10 ** random.uniform(-3, -1)
-
-        # a = 0.02139
-        # b = 0.11385
-        # c = 0.28639
         mp = f'{mp_index:03}'
         mlflow.start_run(run_name=f'{mp}')
         mlflow.log_param('learning rate', learning_rate)
         mlflow.log_param('batch size', bs)
         mlflow.log_param('loss function', 'depth-gradient-ssim')
         mlflow.log_param('model name', mp)
-        mlflow.log_param('model initiated', '030_14_4500')
+        mlflow.log_param('model initiated', '037_19_100')
         mlflow.log_param('alpha', a)
         mlflow.log_param('beta', b)
         mlflow.log_param('theta', c)
